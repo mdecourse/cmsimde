d055a33b5da764c38532180c435cfec1dc4e87d0
mdecourse <mdecourse@gmail.com>
2019-06-06 12:00:17 +0800

    use absolute pythonpath for brython program

--- flaskapp.py
+++ flaskapp.py
@@ -887,7 +887,7 @@ def get_page2(heading, head, edit, get_page_content = None):
     # 假如有 src=/downloads 則換為 src=./../../downloads
     page = [w.replace('src=/downloads', 'src=./../../downloads') for w in page]
     # 假如有 pythonpath:['/static/'] 則換為 
-    page = [w.replace("pythonpath:['/static/']", "pythonpath:['./../cmsimde/static/']") for w in page]
+    #page = [w.replace("pythonpath:['/static/']", "pythonpath:['./../cmsimde/static/']") for w in page]
 
     directory = render_menu2(head, level, page)
     if heading is None:
