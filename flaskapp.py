b09f9838e773c02c0adceb28171aa11a44c005d0
mdecourse <mdecourse@gmail.com>
2020-07-10 11:46:45 +0800

    modified checkMath

--- flaskapp.py
+++ flaskapp.py
@@ -98,22 +98,7 @@ def checkLogin():
 def checkMath():
     outstring = '''
 <!-- 啟用 LaTeX equations 編輯 -->
-<script>
-  (function () {
-  var body = document.body.textContent;
-  if (body.match(/(?:\$|\\\(|\\\[|\\begin\{.*?})/)) {
-    if (!window.MathJax) {
-      window.MathJax = {
-        tex: {
-          inlineMath: {'[+]': [['$', '$']]}
-        }
-      };
-    }
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
-    document.head.appendChild(script);
-  }
-})();
+<script src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js'>
 </script>
     '''
     return outstring
