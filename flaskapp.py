24af2a54e5fed730f4da96fceff8ba633ccbc469
mdecourse <mdecourse@gmail.com>
2020-08-14 13:53:47 +0800

    add content dependent mathjax setup

--- flaskapp.py
+++ flaskapp.py
@@ -104,6 +104,23 @@ def checkMath():
   };
   </script>
   <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>-->
+  <script>
+  (function () {
+  var body = document.body.textContent;
+  if (body.match(/(?:\$|\\\(|\\\[|\\begin\{.*?})/)) {
+    if (!window.MathJax) {
+      window.MathJax = {
+        tex: {
+          inlineMath: {'[+]': [['$', '$']]}
+        }
+      };
+    }
+    var script = document.createElement('script');
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+    document.head.appendChild(script);
+  }
+})();
+</script>
     '''
     return outstring
     
