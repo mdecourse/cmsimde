6273117851beed03afcf8bbfd4503a2f2a9541f1
mdecourse <mdecourse@gmail.com>
2020-08-14 13:42:54 +0800

    comment out checkMath

--- flaskapp.py
+++ flaskapp.py
@@ -98,12 +98,12 @@ def checkLogin():
 def checkMath():
     outstring = '''
 <!-- 啟用 LaTeX equations 編輯 -->
-  <script>
+  <!-- <script>
   MathJax = {
     tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]}
   };
   </script>
-  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>
+  <script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js"></script>-->
     '''
     return outstring
     
