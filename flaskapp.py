7dfa556f7c1f2f8a84edda16d6d3294c747d3fba
mdecourse <mdecourse@gmail.com>
2021-04-24 11:21:06 +0800

    get_page2 for getting self page is not implemented

--- flaskapp.py
+++ flaskapp.py
@@ -929,8 +929,8 @@ def get_page2(heading, head, edit, get_page_content = None):
     page = [w.replace('src="/downloads', 'src="./../downloads') for w in page]
     # 假如有 pythonpath:['/static/'] 則換為 ./../cmsimde/static/
     page = [w.replace("pythonpath:['/static/']", "pythonpath:['./../cmsimde/static/']") for w in page]
-    # 假如有 /get_page 則換為 空字元, 表示要在靜態網頁直接取網頁
-    page = [w.replace('/get_page', '') for w in page]
+    # 假如有 /get_page 則需額外使用 regex 進行字串代換, 表示要在靜態網頁直接取網頁 (尚未完成)
+    #page = [w.replace('/get_page', '') for w in page]
 
     directory = render_menu2(head, level, page)
     if heading is None:
